e04786674ba8df30334b6f1e3cdab005c5e4a2da
yairme <yairstward2013@gmail.com>
2023-01-23 16:15:57 +0100

    Started working on reset/resize maze generation.

--- DTT Maze Generator/Assets/Scripts/MazeGenerator.cs
+++ DTT Maze Generator/Assets/Scripts/MazeGenerator.cs
@@ -15,27 +15,26 @@ public class MazeGenerator : MonoBehaviour
 
     private bool _generatorType = true;
     private List<MazeNode> _nodes = new List<MazeNode>();
+    private List<MazeNode> _currentPath = new List<MazeNode>();
+    private List<MazeNode> _completedNodes = new List<MazeNode>();
+    private List<GameObject> _mazeNodes = new List<GameObject>();
 
-    private int _chosenDirection;
     private MazeNode _chosenNode;
+    private int _chosenDirection;
     private int _randomRange;
-    
+    private Vector2Int _startMazeSize;
+
+    public void Start()
+    {
+        _startMazeSize = mazeSize;
+    }
+
     public void Update()
     {
-        
+     
         text[0].text = sliders[0].value.ToString();
         text[1].text = sliders[1].value.ToString();
 
-    }
-
-    public void ChangeYValue()
-    {
-        mazeSize.y = (int)sliders[0].value;
-    }
-
-    public void ChangeXValue()
-    {
-        mazeSize.x = (int)sliders[1].value;
     }
 
     public void ChangeType()
@@ -45,7 +44,13 @@ public class MazeGenerator : MonoBehaviour
     
     public void GenerateMaze()
     {
-        
+        mazeSize.x = (int)sliders[1].value;
+        mazeSize.y = (int)sliders[0].value;
+        for (var i = 0; i < _nodes.Count; i++)
+        {
+            _nodes[i].ResetWalls();
+            _nodes[i].SetState(NodeState.Available);
+        }
         switch (_generatorType)
         {
             case true:
@@ -61,8 +66,9 @@ public class MazeGenerator : MonoBehaviour
     private void GenerateMaze(Vector2Int size)
     {
         // Create Nods;
-        if (!_nodes.Any() || mazeSize.y != size.y || mazeSize.x != size.x)
+        if (!_nodes.Any() || _startMazeSize != size)
         {
+            _startMazeSize = size;
             for (var x = 0; x < size.x; x++)
             {
                 for (var y = 0; y < size.y; y++)
@@ -73,28 +79,28 @@ public class MazeGenerator : MonoBehaviour
                 }
             }
         }
-        
-        var currentPath = new List<MazeNode>();
-        var completedNodes = new List<MazeNode>();
-        _randomRange = Random.Range(1, _nodes.Count);
+
+        _currentPath = new List<MazeNode>();
+        _completedNodes = new List<MazeNode>();
+        _randomRange = Random.Range(0, _nodes.Count);
         
         // Choose starting node;
-        currentPath.Add(_nodes[_randomRange]);
+        _currentPath.Add(_nodes[_randomRange]);
 
-        while (completedNodes.Count < _nodes.Count)
+        while (_completedNodes.Count < _nodes.Count)
         {
             //Check nodes next to the current node;
             var possibleNextNodes = new List<int>();
             var possibleDirections = new List<int>();
 
-            var currentNodeIndex = _nodes.IndexOf(currentPath[currentPath.Count - 1]);
+            var currentNodeIndex = _nodes.IndexOf(_currentPath[_currentPath.Count - 1]);
             var currentNodeX = currentNodeIndex / size.y;
             var currentNodeY = currentNodeIndex % size.y;
 
             if (currentNodeX < size.x - 1)
             {
-                if (!completedNodes.Contains(_nodes[currentNodeIndex + size.y]) &&
-                    !currentPath.Contains(_nodes[currentNodeIndex + size.y]))
+                if (!_completedNodes.Contains(_nodes[currentNodeIndex + size.y]) &&
+                    !_currentPath.Contains(_nodes[currentNodeIndex + size.y]))
                 {
                     possibleDirections.Add(1);
                     possibleNextNodes.Add(currentNodeIndex + size.y);
@@ -102,8 +108,8 @@ public class MazeGenerator : MonoBehaviour
             }
             if (currentNodeX > 0)
             {
-                if (!completedNodes.Contains(_nodes[currentNodeIndex - size.y]) &&
-                    !currentPath.Contains(_nodes[currentNodeIndex - size.y]))
+                if (!_completedNodes.Contains(_nodes[currentNodeIndex - size.y]) &&
+                    !_currentPath.Contains(_nodes[currentNodeIndex - size.y]))
                 {
                     possibleDirections.Add(2);
                     possibleNextNodes.Add(currentNodeIndex - size.y);
@@ -112,8 +118,8 @@ public class MazeGenerator : MonoBehaviour
             
             if (currentNodeY < size.y - 1)
             {
-                if (!completedNodes.Contains(_nodes[currentNodeIndex + 1]) &&
-                    !currentPath.Contains(_nodes[currentNodeIndex + 1]))
+                if (!_completedNodes.Contains(_nodes[currentNodeIndex + 1]) &&
+                    !_currentPath.Contains(_nodes[currentNodeIndex + 1]))
                 {
                     possibleDirections.Add(3);
                     possibleNextNodes.Add(currentNodeIndex + 1);
@@ -122,50 +128,49 @@ public class MazeGenerator : MonoBehaviour
             
             if (currentNodeY > 0)
             {
-                if (!completedNodes.Contains(_nodes[currentNodeIndex - 1]) &&
-                    !currentPath.Contains(_nodes[currentNodeIndex - 1]))
+                if (!_completedNodes.Contains(_nodes[currentNodeIndex - 1]) &&
+                    !_currentPath.Contains(_nodes[currentNodeIndex - 1]))
                 {
                     possibleDirections.Add(4);
                     possibleNextNodes.Add(currentNodeIndex - 1);
                 }
             }
-            
+
             // Choose next node;
             if (possibleDirections.Count > 0)
             {
                 _chosenDirection = Random.Range(0, possibleDirections.Count);
-                var chosenNode = _nodes[possibleNextNodes[_chosenDirection]];
-                chosenNode.ResetWalls();
+                _chosenNode = _nodes[possibleNextNodes[_chosenDirection]];
 
                 switch (possibleDirections[_chosenDirection])
                 {
                     case 1:
-                        chosenNode.RemoveWall(1);
-                        currentPath[currentPath.Count - 1].RemoveWall(0);
+                        _chosenNode.RemoveWall(1);
+                        _currentPath[_currentPath.Count - 1].RemoveWall(0);
                         break;
                     case 2:
-                        chosenNode.RemoveWall(0);
-                        currentPath[currentPath.Count - 1].RemoveWall(1);
+                        _chosenNode.RemoveWall(0);
+                        _currentPath[_currentPath.Count - 1].RemoveWall(1);
                         break;
                     case 3:
-                        chosenNode.RemoveWall(3);
-                        currentPath[currentPath.Count - 1].RemoveWall(2);
+                        _chosenNode.RemoveWall(3);
+                        _currentPath[_currentPath.Count - 1].RemoveWall(2);
                         break;
                     case 4:
-                        chosenNode.RemoveWall(2);
-                        currentPath[currentPath.Count - 1].RemoveWall(3);
+                        _chosenNode.RemoveWall(2);
+                        _currentPath[_currentPath.Count - 1].RemoveWall(3);
                         break;
                 }
-                currentPath.Add(chosenNode);
+                _currentPath.Add(_chosenNode);
             }
             else
             {
-                completedNodes.Add(currentPath[currentPath.Count - 1]);
-                currentPath.RemoveAt(currentPath.Count - 1);
+                _completedNodes.Add(_currentPath[_currentPath.Count - 1]);
+                _currentPath.RemoveAt(_currentPath.Count - 1);
             }
         }
     }
-    
+
     private IEnumerator FollowGenerateMaze(Vector2Int size)
     {
 
@@ -186,28 +191,28 @@ public class MazeGenerator : MonoBehaviour
             }
         }
 
-        var currentPath = new List<MazeNode>();
-        var completedNodes = new List<MazeNode>();
+        _currentPath = new List<MazeNode>();
+        _completedNodes = new List<MazeNode>();
         _randomRange = Random.Range(1, _nodes.Count);
         
         // Choose starting node;
-        currentPath.Add(_nodes[_randomRange]);
-        currentPath[0].SetState(NodeState.Current);
+        _currentPath.Add(_nodes[_randomRange]);
+        _currentPath[0].SetState(NodeState.Current);
 
-        while (completedNodes.Count < _nodes.Count)
+        while (_completedNodes.Count < _nodes.Count)
         {
             //Check nodes next to the current node;
             var possibleNextNodes = new List<int>();
             var possibleDirections = new List<int>();
 
-            var currentNodeIndex = _nodes.IndexOf(currentPath[currentPath.Count - 1]);
+            var currentNodeIndex = _nodes.IndexOf(_currentPath[_currentPath.Count - 1]);
             var currentNodeX = currentNodeIndex / size.y;
             var currentNodeY = currentNodeIndex % size.y;
 
             if (currentNodeX < size.x - 1)
             {
-                if (!completedNodes.Contains(_nodes[currentNodeIndex + size.y]) &&
-                    !currentPath.Contains(_nodes[currentNodeIndex + size.y]))
+                if (!_completedNodes.Contains(_nodes[currentNodeIndex + size.y]) &&
+                    !_currentPath.Contains(_nodes[currentNodeIndex + size.y]))
                 {
                     possibleDirections.Add(1);
                     possibleNextNodes.Add(currentNodeIndex + size.y);
@@ -215,8 +220,8 @@ public class MazeGenerator : MonoBehaviour
             }
             if (currentNodeX > 0)
             {
-                if (!completedNodes.Contains(_nodes[currentNodeIndex - size.y]) &&
-                    !currentPath.Contains(_nodes[currentNodeIndex - size.y]))
+                if (!_completedNodes.Contains(_nodes[currentNodeIndex - size.y]) &&
+                    !_currentPath.Contains(_nodes[currentNodeIndex - size.y]))
                 {
                     possibleDirections.Add(2);
                     possibleNextNodes.Add(currentNodeIndex - size.y);
@@ -225,8 +230,8 @@ public class MazeGenerator : MonoBehaviour
             
             if (currentNodeY < size.y - 1)
             {
-                if (!completedNodes.Contains(_nodes[currentNodeIndex + 1]) &&
-                    !currentPath.Contains(_nodes[currentNodeIndex + 1]))
+                if (!_completedNodes.Contains(_nodes[currentNodeIndex + 1]) &&
+                    !_currentPath.Contains(_nodes[currentNodeIndex + 1]))
                 {
                     possibleDirections.Add(3);
                     possibleNextNodes.Add(currentNodeIndex + 1);
@@ -235,49 +240,48 @@ public class MazeGenerator : MonoBehaviour
             
             if (currentNodeY > 0)
             {
-                if (!completedNodes.Contains(_nodes[currentNodeIndex - 1]) &&
-                    !currentPath.Contains(_nodes[currentNodeIndex - 1]))
+                if (!_completedNodes.Contains(_nodes[currentNodeIndex - 1]) &&
+                    !_currentPath.Contains(_nodes[currentNodeIndex - 1]))
                 {
                     possibleDirections.Add(4);
                     possibleNextNodes.Add(currentNodeIndex - 1);
                 }
             }
-            
+
             // Choose next node;
             if (possibleDirections.Count > 0)
             {
                 _chosenDirection = Random.Range(0, possibleDirections.Count);
                 _chosenNode = _nodes[possibleNextNodes[_chosenDirection]];
-                _chosenNode.ResetWalls();
 
                 switch (possibleDirections[_chosenDirection])
                 {
                     case 1:
                         _chosenNode.RemoveWall(1);
-                        currentPath[currentPath.Count - 1].RemoveWall(0);
+                        _currentPath[_currentPath.Count - 1].RemoveWall(0);
                         break;
                     case 2:
                         _chosenNode.RemoveWall(0);
-                        currentPath[currentPath.Count - 1].RemoveWall(1);
+                        _currentPath[_currentPath.Count - 1].RemoveWall(1);
                         break;
                     case 3:
                         _chosenNode.RemoveWall(3);
-                        currentPath[currentPath.Count - 1].RemoveWall(2);
+                        _currentPath[_currentPath.Count - 1].RemoveWall(2);
                         break;
                     case 4:
                         _chosenNode.RemoveWall(2);
-                        currentPath[currentPath.Count - 1].RemoveWall(3);
+                        _currentPath[_currentPath.Count - 1].RemoveWall(3);
                         break;
                 }
-                currentPath.Add(_chosenNode);
+                _currentPath.Add(_chosenNode);
                 _chosenNode.SetState(NodeState.Current);
 
                 yield return new WaitForSeconds(0.05f);
             }
             else
             {
-                completedNodes.Add(currentPath[currentPath.Count - 1]);
-                currentPath.RemoveAt(currentPath.Count - 1);
+                _completedNodes.Add(_currentPath[_currentPath.Count - 1]);
+                _currentPath.RemoveAt(_currentPath.Count - 1);
             }
         }
     }
